21ea71e2e9b1c839da538023120f527f1a880a09
ReenataMay <reenvma@gmail.com>
2026-06-24 19:10:40 +0700

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -3,7 +3,6 @@ import sqlite3
 def init_db():
     conn = sqlite3.connect("carsweb.db")
     cursor = conn.cursor()
-    # Membuat tabel jika belum ada
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS cars (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -14,7 +13,6 @@ def init_db():
         )
     """)
     
-    # Cek apakah tabel kosong, jika ya, isi data contoh
     cursor.execute("SELECT COUNT(*) FROM cars")
     if cursor.fetchone()[0] == 0:
         cursor.execute("INSERT INTO cars (merk, model, tahun, harga) VALUES ('Toyota', 'Avanza', 2022, 250000000)")
